f1e26e3839d5231476a2306ab8d260c4702c9d55
Antoine Stalin <astalin@norsys.fr>
2017-03-16 09:50:56 +0100

    fix(fabriq) : wrong files....

--- js/index.js
+++ js/index.js
@@ -1,34 +1,7 @@
 'use strict';
 
-// 19:18
+function liquide(i){
+	$('#Liquid'+i).addClass('strokeIt');
+	$('#LiquidInside'+i).addClass('fillIt');
+}
 
-(function ($) {
-
-  var $coin = $('#coin'),
-      $mainBulb = $('#mainBulb'),
-      $liquids = $('.liquid'),
-      $Liquid01 = $('#Liquid1'),
-      $Liquid02 = $('#Liquid2'),
-      $Liquid03 = $('#Liquid3'),
-      $Liquid04 = $('#Liquid4'),
-      $Liquid05 = $('#Liquid5'),
-      $Liquid06 = $('#Liquid6'),
-      $Liquid07 = $('#Liquid7'),
-      $Liquid08 = $('#Liquid8'),
-      $Liquid09 = $('#Liquid9'),
-      mainTl = new TimelineMax();
-
-  function clearStage() {
-    var clearTl = new TimelineMax();
-
-    clearTl.set($coin, { x: -90, y: 120, scale: 0.5, transformOrigin: 'center center' }).set($mainBulb, { fill: '#fff' }).set($liquids, { stroke: '#fff' });
-
-    return clearTl;
-  }
-
-  function initTimeline() {
-    mainTl.add(clearStage());
-  }
-
-  initTimeline();
-})(jQuery);
